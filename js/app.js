187b53f516934e716e31d4c31456572188f45817
simarjot <simarjot0032@gmail.com>
2023-08-08 01:07:47 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-document.querySelector('.mainscreen').style.backgroundColor='rgb(0,0,0,0.2)'
+document.querySelector('.mainscreen').style.backgroundColor='rgb(0,0,0,0.47)'
 let menu=document.querySelector('.menu');
 let menubutton=document.querySelector('.fa-bars');
 menubutton.addEventListener("click",function(){
-    // header.style.height='fit-content';
+  
 menu.classList.toggle('hide');
 
 })
