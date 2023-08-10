653d978deca2dd0ebef189345208c636a6dd15e1
simarjot <simarjot0032@gmail.com>
2023-08-11 01:38:53 +0530

    update

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+document.querySelector('.mainscreen').style.backgroundColor="rgba(0,0,0,0.47)";
+let menubutton=document.querySelector('.fa-bars');
+let menu=document.querySelector('.menu');
+
+menubutton.addEventListener("click",function(){
+menu.classList.toggle('hide');
+
+if(menubutton.classList.contains('fa-bars')){
+    menubutton.classList.add('fa-xmark');
+    menubutton.classList.remove('fa-bars');
+}
+else{
+    menubutton.classList.add('fa-bars');
+    menubutton.classList.remove('fa-xmark')
+}
+});
